4cd24bebc3aa68df069d74e81c9952fcc39a7d9a
aqcd <aquajet173@gmail.com>
2021-03-13 20:31:16 +0800

    port old to new

---
+++ Valebat/Valebat/Map/MapObject/StaticMapObject.swift
@@ -0,0 +1,10 @@
+//
+//  StaticMapObject.swift
+//  Valebat
+//
+//  Created by Jing Lin Shi on 11/3/21.
+//
+
+protocol StaticMapObject : MapObject {
+    
+}
---
+++ Valebat/Valebat/Map/Util/MapObjectEnum.swift
@@ -0,0 +1,10 @@
+//
+//  MapObjectEnum.swift
+//  Valebat
+//
+//  Created by Jing Lin Shi on 11/3/21.
+//
+
+enum MapObjectEnum: String {
+    case ROCK
+}
