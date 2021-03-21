a637157de8ee21458206475e48f59b196100496b
theyifan <zhang.yifan@u.nus.edu>
2021-03-21 12:02:07 +0800

    remove unused files

--- Valebat/Valebat/Components/ContactNotifiableProtocol.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ContactNotifiableProtocol.swift
-//  Valebat
-//
-//  Created by Aloysius Lim on 16/3/21.
-//
-
-import GameplayKit
--- Valebat/Valebat/Components/InstantDamageComponent.swift
+++ Valebat/Valebat/Components/InstantDamageComponent.swift
@@ -10,6 +10,7 @@ import GameplayKit
 class InstantDamageComponent: DamageComponent, ContactBeginObserver {
 
     var contacted = false
+    
     func contactDidBegin(with entity: GKEntity) {
         if contacted {
             return
@@ -24,10 +25,12 @@ class InstantDamageComponent: DamageComponent, ContactBeginObserver {
             }
         }
     }
+    
     override func didAddToEntity() {
         entity?.component(ofType: PhysicsComponent.self)?.contactBeginObservers[ObjectIdentifier(self)] = self
         super.didAddToEntity()
     }
+    
     override func willRemoveFromEntity() {
         entity?.component(ofType: PhysicsComponent.self)?.contactBeginObservers
             .removeValue(forKey: ObjectIdentifier(self))
