924e0235648451f785999541e336cef0c5609c14
theyifan <zhang.yifan@u.nus.edu>
2021-04-09 22:09:12 +0800

    add constraints

--- Valebat/Valebat/MenuViewController.swift
+++ Valebat/Valebat/MenuViewController.swift
@@ -8,8 +8,32 @@
 import UIKit
 
 class MenuViewController: UIViewController {
-    
+
+    @IBOutlet weak var newGameButton: UIImageView!
+
     override func viewDidLoad() {
+        let value = UIInterfaceOrientation.landscapeLeft.rawValue
+        UIDevice.current.setValue(value, forKey: "orientation")
+
+        let newGameTapGesture = UITapGestureRecognizer(target: self, action: #selector(newGameTapped))
+        newGameButton.addGestureRecognizer(newGameTapGesture)
+        newGameButton.isUserInteractionEnabled = true
     }
-    
+
+    @objc func newGameTapped(gesture: UIGestureRecognizer) {
+        print("hey")
+    }
+
+    override var shouldAutorotate: Bool {
+        return false
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+      return .landscape
+    }
+
 }
