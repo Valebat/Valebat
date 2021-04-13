cbb41ea8a9abee790b51dcf48c37a0690c26dfbe
pooty <aloysiuslim.dw@gmail.com>
2021-04-13 23:45:12 +0800

    Revert "Revert "Merge remote-tracking branch 'origin/restart-fix'""
    This reverts commit 274308de517633093821da0c57c52dac5f227d20.

---
+++ Valebat/Valebat/HeadsUpDisplay/UserInputHUD/OutcomeButton.swift
@@ -0,0 +1,34 @@
+//
+//  WinButton.swift
+//  Valebat
+//
+//  Created by Sreyans Sipani on 13/4/21.
+//
+
+import SpriteKit
+
+class OutcomeButton: SKSpriteNode, UserInputNodeProtocol {
+
+    weak var userInputDelegate: UserInputDelegate?
+    let restartButton: RestartButton = RestartButton()
+
+    init() {
+        let texture = SKTexture(imageNamed: "win-pop")
+        super.init(texture: texture, color: .brown, size: HUDConstants.outcomeButtonSize)
+        self.isUserInteractionEnabled = false
+        self.position = HUDConstants.outcomeButtonHiddenPos
+        self.addChild(restartButton)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func popUp() {
+        self.run(SKAction.move(to: HUDConstants.outcomeButtonPos, duration: 1))
+    }
+
+    func goBackDown() {
+        self.position = HUDConstants.outcomeButtonHiddenPos
+    }
+}
