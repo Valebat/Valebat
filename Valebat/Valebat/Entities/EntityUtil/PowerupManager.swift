4f4b256a00af684c672fde1002680c974ffe3f57
aqcd <aquajet173@gmail.com>
2021-05-02 20:42:17 +0800

    set max value for powers

--- Valebat/Valebat/Entities/EntityUtil/PowerupManager.swift
+++ Valebat/Valebat/Entities/EntityUtil/PowerupManager.swift
@@ -5,17 +5,24 @@
 //  Created by Jing Lin Shi on 1/4/21.
 //
 
+import SpriteKit
+
 class PowerupManager {
     let playerModifiers = PlayerModifiers()
+
+    let maxPlayerDamageMultiplier: CGFloat = 2.0
+    let maxPlayerSpeedMultiplier: CGFloat = 2.0
 
     func collectedPowerup(_ powerup: PowerupEnum, player: PlayerEntity) {
         switch powerup {
         case .heal:
             player.component(ofType: HealthComponent.self)?.healDamage(5.0)
         case .damage:
-            self.playerModifiers.playerDamageMultiplier *= 1.2
+            self.playerModifiers.playerDamageMultiplier = min(self.playerModifiers.playerDamageMultiplier * 1.2,
+                                                              maxPlayerDamageMultiplier)
         case .speed:
-            self.playerModifiers.playerSpeedMultiplier *= 1.2
+            self.playerModifiers.playerSpeedMultiplier = min(self.playerModifiers.playerSpeedMultiplier * 1.2,
+                                                             maxPlayerSpeedMultiplier)
         }
     }
 
